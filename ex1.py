654c8fedfbc0de6243434fd15a9450d35034ef04
canjiang <rencanjiang@gmail.com>
2015-09-16 22:23:18 +0800

    -

--- ex1.py
+++ ex1.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*- 
+
 # Exercise 1. 
 # learn saving a .py file, and use cmd python ex1.py to run this file.
 # learn the string '' and ""
@@ -70,4 +72,47 @@ e12="r"
 # watch that comma at the end, try removing it to see what happens
 print e1 + e2 + e3 + e4 + e5 + e6, 
 print e7 + e8 + e9 + e10 + e11 + e12 
+# we have see above that , is used in print to connect two strings.
+
+# Exercise 8, more printing 
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4) 
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True) 
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.", 
+	"That you could type up right.", 
+	"But id didn't sing.",
+	"So I said goodnight."
+) 
+print "中文"
+# we need to put coding: utf-8 at the top of file, or it complain the non-ascii
+
+# multi lines string 
+print "a\nb"
+print """ 
+line1
+line2
+""" 
+
+# Exercise 10
+# we saw that ' and " is used for sttring. if we want to print ' or ", 
+# we need to use \' and \", these are Escape Sequences, 
+# other Escape Sequences include \n \t \\ etc.
+
+# Exercise 11. Asking question. 
+print "How much do you weight?", 
+weight = raw_input()  
+# pay attention to the , 
+ 
+print "So, you are %r heavy." % weight
+
+height = raw_input("How tall are you?")
+print "So, you are %r tall." % height 
+
+# to learn raw_input() function, you can use:
+# python -m pydoc raw_input 
+# it will print some help information. 
 
